134547077aa4470c2f6413ae14f7f17233258802
Jillian Tankersley <jillian.emilie@gmail.com>
2016-03-11 22:11:23 -0500

    added nacho-meter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,5 +12,6 @@ class WelcomeController < ApplicationController
   	@examples = Example.where(subject_id: @subject.id)
   	@test = Quiz.where(subject_id: @subject.id).first
     @answer_box = [@test.wrong1, @test.wrong2, @test.right].shuffle
+    @nachos = @subject.id
   end
 end
