be8bcf307915f8e9a434d8e506b99ecca290dc65
Jillian Tankersley <jillian.emilie@gmail.com>
2016-03-17 16:07:56 -0400

    added 2 subjects

---
+++ app/admin/quiz.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Quiz do
+
+	permit_params :question, :wrong1, :wrong2, :right, :confirmation, :clue, :video, :subject_id
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end
