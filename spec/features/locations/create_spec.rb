d60ebf69bbfdb63f3ef535fe1f42af9466c2c5e4
Jillian Tankersley <jillian.emilie@gmail.com>
2016-03-24 16:20:30 -0400

    starting to write test for locations

---
+++ spec/features/locations/create_spec.rb
@@ -0,0 +1,13 @@
+require ‘spec_helper’
+
+describe 'creating locations' do
+	visit '/winner'
+	fill_in "City", with: "Boring"
+	select 
+	click_button "I'm here, where are you?"
+	it 'redirects to alien nachos on success' do
+		expect(page).to have_content("Boring")
+	end
+
+
+end
